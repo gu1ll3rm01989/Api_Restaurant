6aed5020267e33a806e1969a0f192cc3218c7ef5
kmsueldo <kmsueldo@hotmail.com>
2020-04-20 18:49:51 -0300

    My first commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ const sequelize = require ('./app/db.conection')
 sequelize
         .authenticate()
         .then(() => {
-        console.log('Database connection has been established successfully.')
+        console.log(`######################
+######################`)
+        console.log('DATABASE CONNECTION has been established SUCCESSFULLY.')
                             
         server.listen(config.SERVER_PORT, () => {
-        console.log(`Server is running at port ${config.SERVER_PORT}`)
+        console.log(`######################
+###### API REST ######
+######################`)
+        console.log(`SERVER is RUNNING at PORT ${config.SERVER_PORT}!!!`)
             })
         })
         .catch(err => console.error('Unable to connect to the database:', err))
